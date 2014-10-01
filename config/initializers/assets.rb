cdd8fee9fa864350b394051ea4abcbf39e6e2cbe
Julie Boling <julie@dustinboling.com>
2014-09-30 18:30:43 -0700

    change precompile assets settings

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,2 @@
-Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif )
+# Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif )
+Rails.application.config.assets.precompile += %w( charcoal-brush.png )

e9cdf85d1b3c158f3c9578d48a56f1a2769fd17b
Julie Boling <julie@dustinboling.com>
2014-10-01 08:45:57 -0700

    precompile theme assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
-# Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif )
-Rails.application.config.assets.precompile += %w( charcoal-brush.png )
+Rails.application.config.assets.precompile += %w( *.png *.jpg *.jpeg *.gif )
+# Rails.application.config.assets.precompile += %w( charcoal-brush.png )
+# Rails.application.config.assets.precompile += %w( derek-jeter-charcoal-painting-940px.jpg )

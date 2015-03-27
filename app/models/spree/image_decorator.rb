fc9cdd5db1a618efbecf1063043a49020ffa3d11
Dustin Boling <dustin@dustinboling.com>
2015-03-27 00:25:23 -0700

    crop thumbnails from the top, instead of center

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,9 +1,16 @@
 Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
-    :mini => '48x48#', # thumbs under image
-    :small => '150x150#', # images on category view
-    :medium => '460x300#',
-    :product => '580x580>', # Single product page
-    :large => '940x740>' # light box image
+    :mini => '', # thumbs under image
+    :small => '', # images on category view
+    :medium => '',
+    :product => '', # Single product page
+    :large => '' # light box image
+  },
+  :convert_options => { 
+    :mini => "-gravity north -thumbnail 48x48^ -extent 48x48" ,
+    :small => "-gravity north -thumbnail 150x150^ -extent 150x150",
+    :medium => "-gravity north -thumbnail 460x300^ -extent 460x300",
+    :product => "-gravity north -thumbnail 580x580^ -extent 580x580",
+    :large => "-gravity north -thumbnail 940x740^ -extent 940x740"
   }
 end

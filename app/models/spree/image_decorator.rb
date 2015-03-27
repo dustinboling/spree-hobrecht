e9faace83650e896c4e373b4a5aa9621efe8a1b1
Dustin Boling <dustin@dustinboling.com>
2015-03-27 11:31:40 -0700

    Fix syntax error

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -5,8 +5,9 @@ Spree::Image.class_eval do
     :medium => '',
     :product => '', # Single product page
     :large => '' # light box image
-  },
-  :convert_options => { 
+  }
+
+  attachment_definitions[:attachment][:convert_options] = { 
     :mini => "-gravity north -thumbnail 48x48^ -extent 48x48" ,
     :small => "-gravity north -thumbnail 150x150^ -extent 150x150",
     :medium => "-gravity north -thumbnail 460x300^ -extent 460x300",

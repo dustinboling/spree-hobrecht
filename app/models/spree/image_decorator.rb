c9d779ca9580c11fd4fee7fbd9733bbcfd61562a
Dustin Boling <dustin@dustinboling.com>
2015-03-27 21:13:15 -0700

    adjust thumbnail sizing

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -3,23 +3,14 @@ Spree::Image.class_eval do
     :mini => '', # thumbs under image
     :small => '', # images on category view
     :medium => '',
-    :product => '', # Single product page
+    :product => '580x9999^', # Single product page
     :large => '' # light box image
   }
 
   attachment_definitions[:attachment][:convert_options] = { 
     :mini => "-gravity north -thumbnail 48x48^ -extent 48x48" ,
     :small => "-gravity north -thumbnail 150x150^ -extent 150x150",
-    :medium => "-gravity north -thumbnail 460x460^ -extent 460x460",
-    :product => "-gravity north -thumbnail 580x580^ -extent 580x580",
-    :large => "-gravity north -thumbnail 940x940^ -extent 940x940"
+    :medium => "-gravity north -thumbnail 460x435^ -extent 460x435",
+    :large => "-gravity north -thumbnail 940x890^ -extent 940x890"
   }
-
-  # attachment_definitions[:attachment][:convert_options] = { 
-  #   :mini => "-gravity north -thumbnail 48x48^ -extent 48x48" ,
-  #   :small => "-gravity north -thumbnail 150x150^ -extent 150x150",
-  #   :medium => "-gravity north -thumbnail 460x300^ -extent 460x300",
-  #   :product => "-gravity north -thumbnail 580x580^ -extent 580x580",
-  #   :large => "-gravity north -thumbnail 940x740^ -extent 940x740"
-  # }
 end
